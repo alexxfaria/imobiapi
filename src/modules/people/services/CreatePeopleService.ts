cc966f05280d29c343db633d469e4f577be67028
Alex Faria <alexxfs@live.com>
2022-06-01 17:07:06 -0300

    alter

--- src/modules/people/services/CreatePeopleService.ts
+++ src/modules/people/services/CreatePeopleService.ts
@@ -1,5 +1,4 @@
 import AppError from '@shared/errors/AppError';
-import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import People from '../typeorm/entities/People';
 import PeopleRepositories from '../typeorm/repositories/PeopleRepositories';
