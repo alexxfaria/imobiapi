eb6316240455b6acf763349e91184e6546a437da
alexxfaria <alexxfs@live.com>
2022-10-11 13:12:53 -0300

    Service delete

--- src/modules/ads/services/DeleteAdsService.ts
+++ src/modules/ads/services/DeleteAdsService.ts
@@ -1,6 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import AdsRepository from '../typeorm/repositories/AdsRepository';
+import PhotoAdsRepository from '../typeorm/repositories/PhotoAdsRepository';
 
 interface IRequest {
   id: string;
@@ -9,6 +10,15 @@ interface IRequest {
 class DeleteAdsService {
   public async execute({ id }: IRequest): Promise<void> {
     const adsRepository = getCustomRepository(AdsRepository);
+    const photoAdsRepository = getCustomRepository(PhotoAdsRepository);
+    const photosAds = await photoAdsRepository.find({
+      where: { id_ads: id },
+    });
+    if (!photosAds) {
+      throw new AppError('Nenhuma imagem encontrada.');
+    }
+    await photoAdsRepository.remove(photosAds);
+
     const ads = await adsRepository.findOne(id);
     if (!ads) {
       throw new AppError('Anúncio não encontrado.');
