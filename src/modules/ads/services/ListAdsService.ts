d4dc548abc2ad4ed9c1041a251031bdd58cc1c67
Alex Faria <alexxfs@live.com>
2022-10-20 19:36:44 -0300

    Add list com photo

--- src/modules/ads/services/ListAdsService.ts
+++ src/modules/ads/services/ListAdsService.ts
@@ -1,12 +1,24 @@
 import { getCustomRepository } from 'typeorm';
 import Ads from '../typeorm/entities/Ads';
 import AdsRepository from '../typeorm/repositories/AdsRepository';
+import PhotoAdsRepository from '../typeorm/repositories/PhotoAdsRepository';
 
 class ListAdsService {
   public async execute(): Promise<Ads[]> {
     const adsRepository = getCustomRepository(AdsRepository);
+    const photosAdsRepository = getCustomRepository(PhotoAdsRepository);
     const ads = await adsRepository.find();
-    return ads;
+    const result = [];
+    for (const anuncio of ads) {
+      const photos = await photosAdsRepository.find({
+        where: {
+          id_ads: anuncio.id,
+        },
+      });
+      result.push({ ...anuncio, photos });
+    }
+
+    return result;
   }
 }
 export default ListAdsService;
